cd47b41bf1da8acf6906c6e4889c74bdb393250b
Fernando Marcon Passos <fmarcon@alumni.usp.br>
2025-09-26 10:21:55 -0300

    mv tmp storage from arrays (RAM) to file (Disk)

--- main.sh
+++ main.sh
@@ -6,12 +6,25 @@ vcf_processed="/home/fernando/lgcm/projects/rare_variants/association-analysis-f
 echo -e "VCF ORIGINAL:\t$(basename $vcf_original)"
 echo -e "VCF PROCESSED:\t$(basename $vcf_processed)"
 
-original=($(bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' $vcf_original | sed 's/^chr//g'| sed 's/\t/_/g' | sort))
-processed=($(bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' $vcf_processed | sed 's/^chr//g'| sed 's/\t/_/g' | sort))
-processed_flipped=($(bcftools query -f '%CHROM\t%POS\t%ALT\t%REF\n' $vcf_processed | sed 's/^chr//g' | sed 's/\t/_/g' | sort))
+# original=($(bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' $vcf_original | sed 's/^chr//g'| sed 's/\t/_/g' | sort))
+# processed=($(bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' $vcf_processed | sed 's/^chr//g'| sed 's/\t/_/g' | sort))
+# processed_flipped=($(bcftools query -f '%CHROM\t%POS\t%ALT\t%REF\n' $vcf_processed | sed 's/^chr//g' | sed 's/\t/_/g' | sort))
 
-common_not_flipped=($(echo "${original[@]}" "${processed[@]}" | tr ' ' '\n' | sort | uniq -d))
-common_flipped=($(echo "${original[@]}" "${processed_flipped[@]}" | tr ' ' '\n' | sort | uniq -d))
+# common_not_flipped=($(echo "${original[@]}" "${processed[@]}" | tr ' ' '\n' | sort | uniq -d))
+# common_flipped=($(echo "${original[@]}" "${processed_flipped[@]}" | tr ' ' '\n' | sort | uniq -d))
 
-echo -e "Not flipped: ${#common_not_flipped[@]}"
-echo -e "Flipped: ${#common_flipped[@]}"
+# echo -e "Not flipped: ${#common_not_flipped[@]}"
+# echo -e "Flipped: ${#common_flipped[@]}"
+
+
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' "$vcf_original" | sed 's/^chr//' | sort > original.txt
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' "$vcf_processed" | sed 's/^chr//' | sort > processed.txt
+bcftools query -f '%CHROM\t%POS\t%ALT\t%REF\n' "$vcf_processed" | sed 's/^chr//' | sort > flipped.txt
+
+not_flipped=$(comm -12 original.txt processed.txt | wc -l)
+flipped=$(comm -12 original.txt flipped.txt | wc -l)
+
+echo "Not flipped: $not_flipped"
+echo "Flipped: $flipped"
+
+rm original.txt processed.txt 
