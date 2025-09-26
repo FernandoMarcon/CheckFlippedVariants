a2bbd67473e3260ff7ae8b03562f51b43ced161d
Fernando Marcon Passos <fmarcon@alumni.usp.br>
2025-09-26 10:13:48 -0300

    first working version

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+vcf_original="/home/fernando/lgcm/projects/rare_variants/association-analysis-for-rare-variants-2025/data/input/original/REHOT_UFRJ_ELSA_original.vcf.gz"
+vcf_processed="/home/fernando/lgcm/projects/rare_variants/association-analysis-for-rare-variants-2025/data/intermediate/03.vars_filtered_kinshipped/REHOT_UFRJ_ELSA_filtered.vcf.gz"
+
+echo -e "VCF ORIGINAL:\t$(basename $vcf_original)"
+echo -e "VCF PROCESSED:\t$(basename $vcf_processed)"
+
+original=($(bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' $vcf_original | sed 's/^chr//g'| sed 's/\t/_/g' | sort))
+processed=($(bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' $vcf_processed | sed 's/^chr//g'| sed 's/\t/_/g' | sort))
+processed_flipped=($(bcftools query -f '%CHROM\t%POS\t%ALT\t%REF\n' $vcf_processed | sed 's/^chr//g' | sed 's/\t/_/g' | sort))
+
+common_not_flipped=($(echo "${original[@]}" "${processed[@]}" | tr ' ' '\n' | sort | uniq -d))
+common_flipped=($(echo "${original[@]}" "${processed_flipped[@]}" | tr ' ' '\n' | sort | uniq -d))
+
+echo -e "Not flipped: ${#common_not_flipped[@]}"
+echo -e "Flipped: ${#common_flipped[@]}"
